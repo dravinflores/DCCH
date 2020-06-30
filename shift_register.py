2c571eab0ae1e0c5b399b3a48022941565008fea
Dravin Flores <dravinflores@gmail.com>
2020-06-30 10:54:33 -0400

    Updated Python program for readability, as well as accuracy.

--- shift_register.py
+++ shift_register.py
@@ -1,180 +1,128 @@
-# This is just so that we can abstract away the idea of a pin.
-class pin:
-    def __init__(self, name, state):
-        self.name = name
-        self.state = state
-
-    def __str__(self):
-        return f"{self.name} is set to {self.state}"
-
-    def set(self, state):
-        self.state = state
+def push_data_to_shift_register(byte_string, data_pin, write_pin):
+    # Go bit by bit.
+    for bit in byte_string:
+        pin = bit
+        write_pin = True
+        write_pin = False
 
 
-class register:
-    def __init__(self):
-        self.state = list()
+def sync_to_manifolds(sync_pin):
+    sync_pin = True
+    sync_pin = False
 
-    def push(self, bit):
-        self.state.append(bit)
 
-    def print(self):
-        for bit in self.state:
-            print(int(bit), end='')
-        print()
-
-    def clear(self):
-        self.state = list()
+def clear_shift_registers(clear_pin):
+    clear_pin = False
+    clear_pin = True
 
 
 def main():
-    # Here I am defining the pins on the PCB itself. These are the default 
-    # states. I will opt to use True/False instead of HIGH/LOW or 0/1.
-    DATA_IN = pin("DATA_IN", False)
-    SYNC = pin("SYNC", False)
-    WRITE = pin("WRITE", False)
-    CLEAR = pin("CLEAR", True)
+    # Simplified Python program to illustrate the usage of the PCB.
 
-    # We're going to pretend this is the shift register.
-    SHIFT = register()
+    # The initial states of the pins.
+    DATA_IN = False
+    SYNC = False
+    WRITE = False
+    CLEAR = True
 
-    # First, we want to clear all the data in the shift registers. This is an
-    # active low signal, meaning to clear it must first be set to False. If it
-    # is not set back to True, then it will constantly clear the register.
-    CLEAR.set(False)
-    CLEAR.set(True)
+    # Clear all shift registers.
+    CLEAR = False
+    CLEAR = True
 
-    # I want to enable the first two tubes. This will have the 8 bit pattern 
-    # 01101100, which will be passed to the DATA_IN pin. In Python, we can
-    # represent the bit string as a list, and then iterate through using a
-    # for loop.
-    byte_str = [False, True, True, False, True, True, False, False]
+    # Labels, for convenience.
+    data_in_label = [
+        # Tubes 8 & 7
+        'OFF', '8B', '8A', 'ENABLE TUBE 8', '7B', '7A', 'ENABLE TUBE 7', 'OFF'
 
-    for bit in byte_str:
-        DATA_IN.set(bit)
-        WRITE.set(True)
-        WRITE.set(False)
+        # Tubes 6 & 5
+        'OFF', '6B', '6A', 'ENABLE TUBE 6', '5B', '5A', 'ENABLE TUBE 5', 'OFF'
 
-        # In this case, the bit has been pushed to the shift register. So
-        # internally, the wiring would push this bit into the shift register
-        # itself, so we wouldn't necessarily see this step.
-        SHIFT.push(bit)
+        # Tubes 4 & 3
+        'OFF', '4B', '4A', 'ENABLE TUBE 4', '3B', '3A', 'ENABLE TUBE 3', 'OFF'
 
-    # Once we've pushed in all the bits sequentially, it is stored in the
-    # register. Now we need to push it to the output.
-    SYNC.set(True)
-    SYNC.set(False)
-    print("Shift Register for tubes 1 & 2: ", end='')
-    SHIFT.print()
-
-
-    # So we've enabled the first two tubes. Now we will need to enable the next
-    # two, without disturbing the first two tubes. So we're going to
-    # get the same string, just with a few ending zeros. Here the rows
-    # correspond to bytes, or rather the first and second shift registers.
-    byte_str = [
-        False, False, False, False, False, False, False, False,
-        False, True, True, False, True, True, False, False,
+        # Tubes 2 & 1
+        'OFF', '2B', '2A', 'ENABLE TUBE 2', '1B', '1A', 'ENABLE TUBE 1', 'OFF'
     ]
 
-    # Don't forget to clear the shift register.
-    CLEAR.set(False)
-    CLEAR.set(True)
-    SHIFT.clear()
-
-    for bit in byte_str:
-        DATA_IN.set(bit)
-        WRITE.set(True)
-        WRITE.set(False)
-
-        # Once again, this would be automatically done for us, in the actual
-        # circuit.
-        SHIFT.push(bit)
-
-    # Now that the shift register has all the data, we just need to push it
-    # to the output.
-    SYNC.set(True)
-    SYNC.set(False)
-    print("Shift Register for tubes 3 & 4: ", end='')
-    SHIFT.print()
-
-
-    # Now for tubes 5 and 6.
-    byte_str = [
+    # We want to connect the first two tubes to the system. This is the data
+    # string that will do that. Connecting tubes 1 and 2.
+    data_string = [
         False, False, False, False, False, False, False, False,
         False, False, False, False, False, False, False, False,
-        False, True, True, False, True, True, False, False,
+        False, False, False, False, False, False, False, False,
+        False, False, True, True, False, True, True, False,
     ]
+    push_data_to_shift_register(data_string, DATA_IN, WRITE)
+    sync_to_manifolds(SYNC)
+    clear_shift_registers(CLEAR)
 
-    # Don't forget to clear the shift register.
-    CLEAR.set(False)
-    CLEAR.set(True)
-    SHIFT.clear()
-
-    for bit in byte_str:
-        DATA_IN.set(bit)
-        WRITE.set(True)
-        WRITE.set(False)
-
-        # Once again, this would be automatically done for us, in the actual
-        # circuit.
-        SHIFT.push(bit)
-
-    # Now that the shift register has all the data, we just need to push it
-    # to the output.
-    SYNC.set(True)
-    SYNC.set(False)
-    print("Shift Register for tubes 5 & 6: ", end='')
-    SHIFT.print()
-
-
-    # Lastly, tubes 7 and 8.
-    byte_str = [
+    # To connect the next two tubes to the system, we will use the following
+    # data string. Connecting tubes 3 and 4.
+    data_string = [
         False, False, False, False, False, False, False, False,
         False, False, False, False, False, False, False, False,
-        False, False, False, False, False, False, False, False,
-        False, True, True, False, True, True, False, False,
+        False, False, True, True, False, True, True, False,
+        False, False, False, False, False, False, False, False
     ]
+    push_data_to_shift_register(data_string, DATA_IN, WRITE)
+    sync_to_manifolds(SYNC)
+    clear_shift_registers(CLEAR)
 
-    # Don't forget to clear the shift register.
-    CLEAR.set(False)
-    CLEAR.set(True)
-    SHIFT.clear()
+    # Connecting tubes 5 and 6.
+    data_string = [
+        False, False, False, False, False, False, False, False,
+        False, False, True, True, False, True, True, False,
+        False, False, False, False, False, False, False, False,
+        False, False, False, False, False, False, False, False
+    ]
+    push_data_to_shift_register(data_string, DATA_IN, WRITE)
+    sync_to_manifolds(SYNC)
+    clear_shift_registers(CLEAR)
 
-    for bit in byte_str:
-        DATA_IN.set(bit)
-        WRITE.set(True)
-        WRITE.set(False)
-
-        # Once again, this would be automatically done for us, in the actual
-        # circuit.
-        SHIFT.push(bit)
-
-    # Now that the shift register has all the data, we just need to push it
-    # to the output.
-    SYNC.set(True)
-    SYNC.set(False)
-    print("Shift Register for tubes 7 & 8: ", end='')
-    SHIFT.print()
-
-
-    # One final clear.
-    CLEAR.set(False)
-    CLEAR.set(True)
-    SHIFT.clear()
+    # Connecting tubes 7 and 8.
+    data_string = [
+        False, False, True, True, False, True, True, False,
+        False, False, False, False, False, False, False, False,
+        False, False, False, False, False, False, False, False,
+        False, False, False, False, False, False, False, False
+    ]
+    push_data_to_shift_register(data_string, DATA_IN, WRITE)
+    sync_to_manifolds(SYNC)
+    clear_shift_registers(CLEAR)
 
     
-    # Of course this process would go on and on, for however many tubes are
-    # connected to the circuit. In this manner, I would expect there are a few
-    # subroutines that can be made, although I am unsure of how to do this in
-    # LabView.
+    # To disconnect a single tube from the system, we can use either of the two
+    # data strings. You would simply just have to pad this data string with
+    # zeros, depending on where the tube was.
+    data_string_even_tube = [
+        False, True, False, True, False, False, False, False
+    ]
 
-    # To disengage an odd tube from the system, this is the bit string:
-    byte_str = [False, True, False, True, False, False, False, False]
+    data_string_odd_tube = [
+        False, False, False, False, True, False, True, False
+    ]
 
-    # To disengage an even tube from the system, this is the bit string:
-    byte_str = [False, False, False, False, True, False, True, False]
+    # For instance, here is the padded data string to disconnect the fifth tube
+    # from the system.
+    data_string = [
+        # Tubes 8 & 7
+        False, False, False, False, False, False, False, False,
 
-    # Of course the numbering goes Tube 1, Tube 2, ... , Tube 8.
+        # Tubes 6 & 5. Notice how I just placed the odd data string here.
+        False, False, False, False, True, False, True, False
 
+        # Tubes 4 & 3
+        False, False, False, False, False, False, False, False,
+
+        # Tubes 2 & 1
+        False, False, False, False, False, False, False, False
+    ]
+    push_data_to_shift_register(data_string, DATA_IN, WRITE)
+    sync_to_manifolds(SYNC)
+    clear_shift_registers(CLEAR)
+
+    # And that's about it.
+
+
+# Call main().
 main()
